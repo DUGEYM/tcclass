81a32b6b64f5165e5e0da4ae48c25c0094bb6e03
DUGEYM <142479269+DUGEYM@users.noreply.github.com>
2024-03-14 15:36:24 +0000

    ths start of the project

--- Classes/Classes/Data/ApplicationDbContext.cs
+++ Classes/Classes/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Calsses.Data
+namespace Classes.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Classes/Classes/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ Classes/Classes/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace Calsses.Data.Migrations
+namespace Classes.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
